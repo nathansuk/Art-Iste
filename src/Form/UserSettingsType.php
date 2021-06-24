ff40510baaf7a3a42be651f319a752de907dfac5
Nathan suk <natthansuk@gmail.com>
2021-06-24 11:32:09 +0200

    Some style changes again (really need to refactor this spaghetti)

--- src/Form/UserSettingsType.php
+++ src/Form/UserSettingsType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Entity\UserSettings;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
